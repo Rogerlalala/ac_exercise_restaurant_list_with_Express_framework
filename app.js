ee8fbfa33c60c56cb7b068db8a2140288a72c67d
Roger Cheng <roger801206@gmail.com>
2020-11-09 18:44:38 +0800

    Initiate

---
+++ app.js
@@ -0,0 +1,22 @@
+// Include package used in project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+// Setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// Setting static files
+app.use(express.static('public'))
+
+// Setting route
+app.get(`/`, (req, res) => {
+  res.render(`index`)
+})
+
+// Start and listen the server
+app.listen(port, () => {
+  console.log(`Express is running on localhost:${port}`)
+})
