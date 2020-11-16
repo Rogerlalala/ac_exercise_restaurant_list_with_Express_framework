0da9731d216c84c27b3cacddc024235958203120
Roger Cheng <roger801206@gmail.com>
2020-11-16 23:24:25 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const Restaurants = require('./models/restaurantList')
+const bodyParser = require('body-parser')
 const port = 3000
 const exphbs = require('express-handlebars')
 //const restaurantsList = require('./restaurant.json')
@@ -11,8 +12,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// Setting static files
+// Setting static files and body-parser
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extend: true }))
 
 // Mongoose
 mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -32,21 +34,52 @@ app.get(`/`, (req, res) => {
     .then(restaurants => res.render(`index`, { restaurants }))
     .catch(error => console.log(error))
 })
-// Show function
-app.get(`/restaurants/:restaurant_id`, (req, res) => {
-  const restaurant = restaurantsList.results.find(restaurant =>
-    restaurant.id.toString() === req.params.restaurant_id
-  )
-  res.render('show', { restaurant: restaurant })
+// New function
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
 })
-// Search function
-app.get(`/search`, (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantsList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+// Create function
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurants.create({
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
+    description
   })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
+// Show function
+//app.get(`/restaurants/:restaurant_id`, (req, res) => {
+//  const restaurant = restaurantsList.results.find(restaurant =>
+//    restaurant.id.toString() === req.params.restaurant_id
+//  )
+//  res.render('show', { restaurant: restaurant })
+//})
+// Search function
+//app.get(`/search`, (req, res) => {
+//  const keyword = req.query.keyword
+//  const restaurants = restaurantsList.results.filter(restaurant => {
+//    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+//  })
+//  res.render('index', { restaurants: restaurants, keyword: keyword })
+//})
+
+
 
 // Start and listen the server
 app.listen(port, () => {
