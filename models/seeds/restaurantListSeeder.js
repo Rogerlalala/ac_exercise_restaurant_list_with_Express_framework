6d01d92410b66821a68065959973c38c5553d273
Roger Cheng <roger801206@gmail.com>
2020-11-16 22:28:25 +0800

    feat: add restaurantList seeder

---
+++ models/seeds/restaurantListSeeder.js
@@ -0,0 +1,26 @@
+// Mongoose
+const mongoose = require('mongoose')
+const RestaurantList = require('../restaurantList')
+const restaurants = require('../../restaurant.json')
+mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  restaurants.results.forEach(restaurant => {
+    RestaurantList.create({
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description
+    })
+  })
+  console.log('done!')
+})
